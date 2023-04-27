ee545f9200946fc147b0b0beb37102c60139d1c2
Tim <timeder92@gmail.com>
2023-04-27 14:02:24 +0200

    add more methods to service

--- client/src/composables/services/dishService.ts
+++ client/src/composables/services/dishService.ts
@@ -7,6 +7,9 @@ const ENDPOINT = "/dishes";
 
 export const useDishes = () => {
   const dishes = ref<Dish[]>([]);
+  const dish = ref<Dish>();
+  const updatedDish = ref<Dish>();
+  const deletedDish = ref<Dish>();
   const loading = ref<boolean>(false);
   const error = ref<string | null>(null);
 
@@ -30,10 +33,77 @@ export const useDishes = () => {
     }
   };
 
+  const clearAllDishes = async () => {
+    loading.value = true;
+    try {
+      await axios.get(url + "/clear");
+    } catch (err) {
+      error.value = "Failed to clear dishes";
+      console.log(err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const getDishById = async (id: string) => {
+    loading.value = true;
+    try {
+      const response = await axios.get(url + "/" + id);
+
+      if (response.data) {
+        dish.value = response.data.data;
+      }
+    } catch (err) {
+      error.value = "Failed to get dish";
+      console.log(err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const insertDish = async (id?: string) => {
+    loading.value = true;
+    try {
+      const response = await axios.put(url, id);
+
+      if (response.data) {
+        updatedDish.value = response.data.data;
+      }
+    } catch (err) {
+      error.value = "Failed to insert dish";
+      console.log(err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const deleteDish = async (id: string) => {
+    loading.value = true;
+    try {
+      const response = await axios.delete(url + "/" + id);
+
+      if (response.data) {
+        deletedDish.value = response.data.data;
+      }
+    } catch (err) {
+      error.value = "Failed to delete dish";
+      console.log(err);
+    } finally {
+      loading.value = false;
+    }
+  };
+
   return {
     dishes,
+    dish,
+    updatedDish,
+    deletedDish,
     loading,
     error,
     getAllDishes,
+    clearAllDishes,
+    getDishById,
+    insertDish,
+    deleteDish,
   };
 };
